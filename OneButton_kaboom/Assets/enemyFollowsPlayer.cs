77b92fbf184d03d4963640472ef37011c1562f6b
Kim <Kim@Kimberlys-MBP.wireless.newschool.edu>
2016-10-25 17:52:45 -0400

    Beginnings of game's design.

---
+++ OneButton_kaboom/Assets/enemyFollowsPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyFollowsPlayer : MonoBehaviour {
+
+	public float speed;
+	private Transform player;
+
+	void Start () {
+
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float z = Mathf.Atan2 ((player.transform.position.y - transform.position.y), (player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+
+		transform.eulerAngles = new Vector3 (0, 0, z);
+
+		GetComponent <Rigidbody> ().AddForce (gameObject.transform.up * speed);
+	
+	}
+}
---
+++ OneButton_kaboom/Assets/spawnButtonRandom.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnButtonRandom : MonoBehaviour {
+
+	public GameObject button;
+	void Start (){
+		//		StartCoroutine ("waitSeconds");
+		//		Instantiate (enemy2, transform.position, transform.rotation);
+		InvokeRepeating("SpawnButton", 0f, 3f);
+	}
+
+	void SpawnEnemy() {
+		Instantiate (button, transform.position, transform.rotation);
+	}
+
+	IEnumerator waitSeconds() {
+		Instantiate (button, transform.position, transform.rotation);
+		yield return new WaitForSeconds (7);
+		Instantiate (button, transform.position, transform.rotation);
+		yield return new WaitForSeconds (9);
+
+
+	}
+}
+
+
