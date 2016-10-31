7a7f51525ac90f871ceeedba7f20152b2ff417ca
Wilson Diaz <diazw291@newschool.edu>
2016-10-31 00:53:46 -0400

    added:  title screen enemy sound background sound FX navmesh(needs work) game over ui(needs work) explosion asset(needs to be placed in scene)

---
+++ OneButton_kaboom/Assets/enemyNav.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyNav : MonoBehaviour {
+	/* 
+	 * if the distance between player and enemy is less than the atk space then navigate to player
+	 *
+	 */
+	public AudioClip audio;
+	private bool hasplayed = false;
+	public Canvas cv;
+	public float atkDist;
+	Transform player;
+	NavMeshAgent nav;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent<NavMeshAgent> ();
+		cv.gameObject.SetActive (false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Vector3.Distance (player.position, transform.position) < atkDist) {
+			nav. SetDestination(player.transform.position);
+		}
+
+		else {
+			return;
+		}
+		Debug.Log (transform.position);
+	}
+
+
+	void OnTriggerEnter(Collider Other)
+	{
+		if (Other.gameObject.transform.tag == "Player" || !hasplayed) 
+		{
+			GetComponent<AudioSource> ().PlayOneShot (audio);
+			hasplayed = true;
+		}
+
+
+	}
+
+		void OnCollisionEnter(Collision col )
+	{
+		if (col.gameObject.transform.tag == "Player") 
+		{
+			cv.gameObject.SetActive(true);
+		
+		}
+
+
+	}
+}
---
+++ OneButton_kaboom/Assets/sceneManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour {
+
+
+	public void restartCurrentScene()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void exit ()
+	{
+		Application.Quit ();
+
+	}
+	public void newGame ()
+	{
+		SceneManager.LoadScene("OneButtonKaboomFAM");
+
+	}
+}
