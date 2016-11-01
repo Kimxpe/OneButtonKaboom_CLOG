00581ec1528ed45a37e34d7ec311d3b21cbccfb7
Kim <Kim@Kimberlys-MBP.home>
2016-10-31 23:37:06 -0400

    Updated start menu, added beginnings of end menu, added a way to "end" game

--- OneButton_kaboom/Assets/sceneManager.cs
+++ OneButton_kaboom/Assets/sceneManager.cs
@@ -4,20 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class sceneManager : MonoBehaviour {
 
-
-	public void restartCurrentScene()
-	{
-		Application.LoadLevel (Application.loadedLevel);
-	}
-
 	public void exit ()
 	{
 		Application.Quit ();
 
 	}
-	public void newGame ()
-	{
-		SceneManager.LoadScene("OneButtonKaboomFAM");
 
-	}
 }
