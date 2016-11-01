aebd27a25420d9a8e43bf3d9b79c5438db57ac21
Kim <Kim@Kimberlys-MBP.home>
2016-11-01 01:27:59 -0400

    Added sounds that were originally lost once again, added different sound to end scene. Finished making the end menu work and all menus

---
+++ OneButton_kaboom/Assets/Scripts/sceneManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour {
+
+	public void exit ()
+	{
+		Application.Quit ();
+
+	}
+
+}
